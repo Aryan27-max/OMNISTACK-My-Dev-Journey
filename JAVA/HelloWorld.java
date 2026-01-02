e8ad9a1efbf43d2b76e52c9bdff2b7fa02c1a0ff
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-01-02 05:55:41 +0530

    intro to basics

--- JAVA/HelloWorld.java
+++ JAVA/HelloWorld.java
@@ -1,8 +1,8 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class HelloWorld{
+    public static void main (String[] args){
+      // this is my 1st java program
+        System.out.println("I like pizza");
+        System.out.println("It's really good");
+        System.out.println("Buy me pizza");
     }
 }
-
-
-// hello world 
