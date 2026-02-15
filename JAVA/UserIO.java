b2255871916669572b8922cf413a67bb7a36da2d
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-02-15 23:31:10 +0530

    Vote java

--- JAVA/UserIO.java
+++ JAVA/UserIO.java
@@ -1,21 +1,44 @@
-
 import java.util.Scanner;
 
-// for user IO we use the java scanner class imported from the Utils 
-// libraray in java 
-
 class UserIO {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-
+        
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
+      
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
 
-        System.out.println("your name is " + name);
-
-
+        if (age < 18){
+            System.out.println("Sorry  " + name + "you are not eligible to vote");
+        }else{
+            System.out.println("Hello  " + name + "you are eligible to vote");
+        }
         sc.close();
     }
 }
 
-//bkekrsg8
+
+
+
+// import java.util.Scanner;
+
+// // for user IO we use the java scanner class imported from the Utils 
+// // libraray in java 
+
+// class UserIO {
+//     public static void main (String[] args){
+//         Scanner sc = new Scanner (System.in);
+
+//         System.out.print("Enter your name: ");
+//         String name = sc.nextLine();
+
+//         System.out.print("enter your age: ");
+//         int age = sc.nextInt();
+
+//         System.out.println("your name is " + name + "you are " + age + " years old ");
+
+//         sc.close();
+//     }
+// }
