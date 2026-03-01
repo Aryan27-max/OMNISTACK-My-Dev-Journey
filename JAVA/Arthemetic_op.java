d4e40ee7fb36f0d51f2960d23de5e811edd2303b
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-03-01 14:22:04 +0530

    Arthemetic op

---
+++ JAVA/Arthemetic_op.java
@@ -0,0 +1,41 @@
+public class Arthemetic_op {
+
+    public static void main (String[] args){
+
+        double x = 10;
+        double y = 3;
+        double z;
+
+        // basic operations - 
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y; (gives the remainder)
+
+        // Augumented Assigment Operators 
+        //(condenses the operation between 2 vars (no need of z))
+        // x += y; means x+y = 13
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+        
+        // increment and decrement Operators
+        // x++;
+        // x--;
+
+        // Mathematical operation in java use the rule [ P E M D A S]
+        // P - parenthesis
+        // E - Exponents
+        // M = Multiplication 
+        // D = Division 
+        // A = Addition 
+        // S = Substraction 
+
+        double result = 3 + 4 * (5 * 7) / 2.0;
+
+        System.out.println(result);
+    }
+    
+}
