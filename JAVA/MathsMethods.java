bb9ea731ea33b6d9429bcdfd1988a88ba269c9cc
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-04-09 22:32:21 +0530

    Enhance MathsMethods class with additional mathematical functions: ceil and floor

--- JAVA/MathsMethods.java
+++ JAVA/MathsMethods.java
@@ -16,7 +16,16 @@ public class MathsMethods {
         // this Math.pow fn is used to raise the power of a number 
         // this works as Math.pow(base, power)
         result = Math.pow(2,3);
-        // this is the mo
+        // this is the modulus or the absolute value method 
+        result = Math.abs(3.14);
+        //this is the sqrt method 
+        result = Math.sqrt(9);
+        // this is rounding off method thi srounds off to the nearest number 
+        result = Math.round(5.14);
+        // this is the celing method i.e. this rounds off to the upper value of the number 
+        result = Math.ceil(5.43);
+        //this is the floor method this rounds down to the lower value of any number 
+        result = Math.floor(8.11) ;
 
         System.out.println(result);
 
